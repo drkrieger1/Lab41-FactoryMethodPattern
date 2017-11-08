14ebb1a1a3385535c62b79e98a58a7962b00b414
drkrieger <eplyushko@gmail.com>
2017-11-08 10:52:49 -0800

    classes built out

---
+++ FactoryMethodPattern/FactoryMethodPattern/Document.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethodPattern
+{
+    public abstract class Document
+    {
+        public string Type { get; set; }
+        public List<string> Pages = new List<string>();
+
+        public void Print()
+        {
+            Console.WriteLine("Printing your documents");
+            foreach(string page in Pages)
+            {
+                Console.WriteLine(page);
+            }
+        }
+    }
+}
